563ddb271cada4fe5918fa25ecdf3d438212f662
Syahmi-Su <syahm.asd@gmail.com>
2021-04-11 22:42:13 +0800

    add

---
+++ aspbet-mvc-beginner/Controllers/UserController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using aspbet_mvc_beginner.Models;
+
+
+namespace aspbet_mvc_beginner.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult AddOrEdit(int id=0)
+        {
+            dbo_User userModel = new dbo_User();
+            return View(userModel);
+        }
+    }
+}
