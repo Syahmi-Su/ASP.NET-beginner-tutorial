c9613367ba194565702b3e3fd2231f6b616d585f
Syahmi-Su <syahm.asd@gmail.com>
2021-04-13 16:08:09 +0800

    Update UserController.cs

--- aspbet-mvc-beginner/Controllers/UserController.cs
+++ aspbet-mvc-beginner/Controllers/UserController.cs
@@ -10,11 +10,24 @@ namespace aspbet_mvc_beginner.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
+        [HttpGet]
         public ActionResult AddOrEdit(int id=0)
         {
             dbo_User userModel = new dbo_User();
             return View(userModel);
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(dbo_User userModel)
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                dbModel.dbo_User.Add(userModel);
+                dbModel.SaveChanges();
+            }
+            ModelState.Clear();
+            ViewBag.SuccessMessage = "Registration Successful. ";
+            return View("AddOrEdit", new dbo_User());
         }
     }
 }
