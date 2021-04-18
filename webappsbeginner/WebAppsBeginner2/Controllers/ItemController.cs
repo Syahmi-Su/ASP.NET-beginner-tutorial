e24f422e9fdd145df77bbf3095d3f05d24b2d04c
Syahmi-Su <syahm.asd@gmail.com>
2021-04-18 23:56:42 +0800

    new file

---
+++ webappsbeginner/WebAppsBeginner2/Controllers/ItemController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppsBeginner2.Data;
+using WebAppsBeginner2.Models;
+
+namespace WebAppsBeginner2.Controllers
+{
+    public class ItemController : Controller
+    {
+        private readonly ApplicationDBContext _db; 
+
+        public ItemController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Item> objList = _db.Items;
+            return View(objList);
+        }
+    }
+}
---
+++ webappsbeginner/WebAppsBeginner2/Data/ApplicationDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppsBeginner2.Models;
+
+namespace WebAppsBeginner2.Data
+{
+    public class ApplicationDBContext :DbContext 
+    {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options)
+        {
+             
+
+        }
+
+        public DbSet<Item> Items { get; set; }
+
+    }
+}
