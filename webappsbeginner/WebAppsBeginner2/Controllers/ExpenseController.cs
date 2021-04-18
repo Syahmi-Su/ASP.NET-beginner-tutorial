e639d89163f9c643db1f44d63ac68add8df53c4e
Syahmi-Su <syahm.asd@gmail.com>
2021-04-19 01:10:52 +0800

    upload

---
+++ webappsbeginner/WebAppsBeginner2/Controllers/ExpenseController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAppsBeginner2.Data;
+using WebAppsBeginner2.Models;
+
+namespace WebAppsBeginner2.Controllers
+{
+    public class ExpenseController : Controller
+    {
+        private readonly ApplicationDBContext _db;
+
+        public ExpenseController(ApplicationDBContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            //numerable<Expense> objList = _db.Expenses;
+            //return View(objList);
+            return View();
+
+        }
+
+        //GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Expense obj)
+        {
+            _db.Expenses.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ webappsbeginner/WebAppsBeginner2/Models/Expense.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppsBeginner2.Models
+{
+    public class Expense
+    {
+        [Key]
+        public int Id { get; set;}
+        [System.ComponentModel.DisplayName("Expense")]
+        public string ExpenseName { get; set; }
+        public int Amount { get; set; }
+        
+
+     
+    }
+}
