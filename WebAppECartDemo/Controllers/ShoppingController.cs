68748d140cfc10f496429d948ece1d696d12e5d2
Syahmi-Su <syahm.asd@gmail.com>
2021-04-27 02:37:04 +0800

    update

--- WebAppECartDemo/Controllers/ShoppingController.cs
+++ WebAppECartDemo/Controllers/ShoppingController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAppECartDemo.Models;
+using WebAppECartDemo.ViewModel;
 
 namespace WebAppECartDemo.Controllers
 {
     public class ShoppingController : Controller
     {
+        private ECartDBEntities objCartDbEntities;
+
+        public ShoppingController()
+        {
+            objCartDbEntities = new ECartDBEntities();
+        }
         // GET: Shopping
         public ActionResult Index()
         {
-            return View();
+            IEnumerable<ShoppingViewModel> listofShoppingViewModels = (from objItem in objCartDbEntities.Items
+                                                                       join
+                                                                       objCate in objCartDbEntities.Categories
+                                                                       on objItem.CategoryId equals objCate.CategoryId
+                                                                       select new ShoppingViewModel()
+                                                                       {
+                                                                           ImagePath = objItem.ImagePath,
+                                                                           ItemName = objItem.ItemName,
+                                                                           Description = objItem.Description,
+                                                                           ItemPrice = objItem.ItemPrice,
+                                                                           ItemId = objItem.ItemId,
+                                                                           Category = objCate.CategoryName,
+                                                                           ItemCode = objItem.ItemCode,
+                                                                       }
+
+                                                                       ).ToList();
+            return View(listofShoppingViewModels);
         }
     }
 }
