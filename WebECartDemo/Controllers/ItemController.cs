91ea8662c33c39b3ee6c2a0bb758468e94a109a4
Syahmi-Su <syahm.asd@gmail.com>
2021-04-27 01:33:44 +0800

    new project

---
+++ WebECartDemo/Controllers/ItemController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebECartDemo.Models;
+using WebECartDemo.ViewModel;
+
+namespace WebECartDemo.Controllers
+{
+    public class ItemController : Controller
+    {
+        private ECartDBEntities objeCartDBEntities;
+        public ItemController()
+        {
+            objeCartDBEntities = new ECartDBEntities();
+        }
+        // GET: Item
+        public ActionResult Index()
+        {
+            ItemViewModel objItemViewModel = new ItemViewModel();
+            objItemViewModel.CategorySelectListItem = (from objCat in objeCartDBEntities.Categories
+                                                       select new SelectListItem()
+                                                       {
+                                                           Text = objCat.CategoryName,
+                                                           Value = objCat.CategoryId.ToString(),
+                                                           Selected = true
+
+                                                       });
+            return View(objItemViewModel);
+        }
+    }
+}
